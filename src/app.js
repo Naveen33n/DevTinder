f3df7d09e83f7163614e07a417b6bbb76b7fc186
Naveen4272 <naveenkumar4272n@gmail.com>
2025-05-09 06:42:29 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@ const express= require("express");
 const app = express();
 
 
-app.use("/user",(req,res)=>{
-  res.send("hello world")
+
+app.use("/test",(req,res)=>{
+  res.send("test ..")
 })
-app.use("/",(req,res)=>{
-  res.send("hi hi")
+
+app.post("/user",(req,res)=>{
+  res.send('data saved')
+})
+
+app.get("/user",(req,res)=>{
+  res.send({firstName:"naveen",
+    lastName:"kumar"
+  })
 })
 app.listen(3000,()=>{
   console.log("listening to the port 3000..");
